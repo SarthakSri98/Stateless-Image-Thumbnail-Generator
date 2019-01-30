05e8a51e9e21da3c5b1b97c3c838eadbe1efafcc
SarthakSri98 <sarthaksri1998@gmail.com>
2019-01-30 21:52:07 +0530

    thumbnail genration is ongit add .git add .

--- backend/controller/imageController.js
+++ backend/controller/imageController.js
@@ -1,33 +1,46 @@
 const fs = require('fs');
 const request = require('request');
 const isImageUrl = require('is-image-url');
+var im = require('imagemagick');
 
-var i=1;
-var download = function(uri, filename, callback){
-request.head(uri, function(err, res, body){
-console.log('content-type:', res.headers['content-type']);
-console.log('content-length:', res.headers['content-length']);
 
-request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-});
+
+var i = 1;
+
+
+var download = function (uri, filename, callback) {
+  request.head(uri, function (err, res, body) {
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
 };
 
 
-exports.returnThumbnail = function(req,res,next){
-console.log(isImageUrl(req.body.imageUrl));
-if(isImageUrl(req.body.imageUrl))
-{
- download(req.body.imageUrl, './backend/controller/'+i+'.png', function(){
-  console.log('done');
-  res.status(200).json({
-      message:"Process going on",
-      imagePath : './backend/controller/'+i+'.png'
-  })
-  i++;
-});
-}
+exports.returnThumbnail = function (req, res, next) {
+    
+  console.log(req.body);
+  if (isImageUrl(req.body.imageUrl)) {
+    download(req.body.imageUrl, './backend/controller/full.png', function () {
+      console.log('done');
 
-else{
-    console.log("lauda lelo");
-}
+      im.resize({
+        srcPath: './backend/controller/full.png',
+        dstPath: './backend/images/thumbnail.png',
+        width: 50,
+        height: 50
+      }, function(err, stdout, stderr){
+        // if (err) throw err;
+        console.log('resized image to fit within 50x50px');
+    });
+
+      res.status(200).json({
+        message: "Process going on",
+        imagePath: 'backend/images/thumbnail.png'
+      });
+      i++;
+    });
+  } else {
+    console.log("Image not found");
+  }
 }
--- src/app/thumb-nail/thumb-nail.component.ts
+++ src/app/thumb-nail/thumb-nail.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl,FormControlName  } from '@angular/forms';
 import { BaseService } from '../services/base.service';
 import { Router } from '@angular/router';
+import { applyOperation } from 'fast-json-patch'
+
+
 @Component({
   selector: 'app-thumb-nail',
   templateUrl: './thumb-nail.component.html',
   styleUrls: ['./thumb-nail.component.less']
 })
 export class ThumbNailComponent implements OnInit {
+  jsonpatch = require('fast-json-patch')
   imageForm;
-  image
+  image;
+  imagePath:string="#";
   constructor(private _base: BaseService, private router:Router) { }
 
   ngOnInit() {
@@ -20,13 +25,18 @@ export class ThumbNailComponent implements OnInit {
 
   submit()
   {
-    this._base.submitImage(this.imageForm.value).subscribe(res=>{
+    var doc = this.imageForm.value;
+var patch = [
+  { op: "add", path: "/token", value: localStorage.getItem('token') },
+];
+doc = this.jsonpatch.applyPatch(doc, patch).newDocument;
+
+    this._base.submitImage(doc).subscribe(res=>{
       console.log(res);
       this.image = document.getElementById('image');
       this.image.style.display='block';
       console.log(this.image.style.display);
-      this.image.src = res.imagePath;
-      console.log(this.image.src);
+      this.imagePath = res.imagePath;
     })
   }
 
